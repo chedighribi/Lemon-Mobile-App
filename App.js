bd58a9e68231512a4a85b08d785ba557052f422d
chedi.ghribi <chedi.ghribi@walterre.fr>
2024-01-07 16:35:49 +0100

    feat: adding welcome screen

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { View } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
+import WelcomeScreen from './components/WelcomeScreen';
 
 export default function App() {
   return (
@@ -11,6 +12,8 @@ export default function App() {
           backgroundColor: '#495E57',
         }}>
         <LittleLemonHeader />
+
+        <WelcomeScreen />
       </View>
       <View style={{ backgroundColor: '#495E57' }}>
         <LittleLemonFooter />
